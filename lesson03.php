b260d65bb9cdc1ef35e8ca1580ea85787373f926
tomo4322 <tomotaka4322@gmail.com>
2022-12-29 16:57:31 +0900

    second corriculum

---
+++ lesson03.php
@@ -0,0 +1,18 @@
+﻿<?php
+// 以下をfor文を使用して表示してください。
+
+// 98
+// 765
+// 4321
+
+$input = 9;
+$addNumber = 2;
+
+for ($x = 1; $x <= 3; $x ++) {
+    for ($y = 1; $y <= $addNumber; $y ++) {
+        echo $input;
+        $input --;
+    }
+    $addNumber ++;
+    echo "<br/>\n";
+}
